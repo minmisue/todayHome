800d5a80fc208b88b9a501fff5490ba308815393
soojinsmacbook <soojinsmacbook@localhost>
2023-06-19 21:35:09 +0900

    member, notification 매퍼 완

--- todayHome/src/main/java/com/sp/app/domain/member/Notification.java
+++ todayHome/src/main/java/com/sp/app/domain/member/Notification.java
@@ -1,18 +1,27 @@
 package com.sp.app.domain.member;
 
 public class Notification {
-	private int notificationId;
+	private Long notificationId;
+	private Long memberId;
 	private int type;
 	private String message;
 	private String regDate;
 	private boolean isRead;
+	private String parameter1;
+	private String parameter2;
+	private String parameter3;
 	
-	
-	public int getNotificationId() {
+	public Long getNotificationId() {
 		return notificationId;
 	}
-	public void setNotificationId(int notificationId) {
+	public void setNotificationId(Long notificationId) {
 		this.notificationId = notificationId;
+	}
+	public Long getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
 	}
 	public int getType() {
 		return type;
@@ -38,6 +47,25 @@ public class Notification {
 	public void setRead(boolean isRead) {
 		this.isRead = isRead;
 	}
+	public String getParameter1() {
+		return parameter1;
+	}
+	public void setParameter1(String parameter1) {
+		this.parameter1 = parameter1;
+	}
+	public String getParameter2() {
+		return parameter2;
+	}
+	public void setParameter2(String parameter2) {
+		this.parameter2 = parameter2;
+	}
+	public String getParameter3() {
+		return parameter3;
+	}
+	public void setParameter3(String parameter3) {
+		this.parameter3 = parameter3;
+	}
+	
 	
 	
 }
--- todayHome/src/main/java/com/sp/app/member/notification/NotificationRepository.java
+++ todayHome/src/main/java/com/sp/app/member/notification/NotificationRepository.java
@@ -9,11 +9,10 @@ public interface NotificationRepository {
 	void createNotification(Notification notification) throws Exception;
 	void updateNotification(Notification notification) throws Exception;
 	void deleteNotification(Notification notification) throws Exception;
-	List<Notification> getNotificationList(Long memberId) throws Exception;
-	// 회원의 알림 개수
-	int getNotificationCount(Long memberId) throws Exception;
+	List<Notification> getNotReadNotificationList(Long memberId) throws Exception;
 	// 회원의 안읽은 알림 개수
 	int getNotReadNotificationCount(Long memberId) throws Exception;
+	
 	
 	
 }

6ec4e8625a81d6e0af4f68ffaffabf3f6c420f4f
dongk <dongk@DESKTOP-SKY>
2023-06-25 15:09:08 +0900

    impl 수정

--- todayHome/src/main/java/com/sp/app/seller/adjustment/AdjustmentServiceImpl.java
+++ todayHome/src/main/java/com/sp/app/seller/adjustment/AdjustmentServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sp.app.domain.board.NoticeBoard;
 import com.sp.app.domain.seller.SellerAdjustment;
 
 @Service

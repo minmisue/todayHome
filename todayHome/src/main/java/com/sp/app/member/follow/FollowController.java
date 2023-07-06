da71254450cab93900a65b957b0b8fed4aa36990
soojinKim <gogogo960922@gmail.com>
2023-07-06 22:25:44 +0900

    컨트롤러

--- todayHome/src/main/java/com/sp/app/member/follow/FollowController.java
+++ todayHome/src/main/java/com/sp/app/member/follow/FollowController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -163,7 +164,7 @@ public class FollowController {
 		return false;
 	}
 	
-	@GetMapping("community/follow")
+	@GetMapping("community/follow/feed")
 	public String followingForm(Model model, HttpSession session) {
 		SessionInfo sessionInfo = (SessionInfo) session.getAttribute("sessionInfo");
 		Long memberId = null;
@@ -204,7 +205,7 @@ public class FollowController {
 		return "community/following/following";		
 	}
 	
-	@GetMapping("community/follow")
+	@PostMapping("community/follow/feed")
 	public String followingSubmit(Model model, HttpSession session) {
 		SessionInfo sessionInfo = (SessionInfo) session.getAttribute("sessionInfo");
 		Long memberId = null;
